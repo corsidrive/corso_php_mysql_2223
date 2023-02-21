7f5fe25e84f133f8e0b23bc71fad3b23151bde7f
Mona Lisa <sarassoroberto@gmail.com>
2023-02-21 18:04:23 +0100

    OKKKEY

--- form_in_php/class/validator/ValidateDate.php
+++ form_in_php/class/validator/ValidateDate.php
@@ -2,6 +2,24 @@
 
 class ValidateDate implements Validable {
 
+    public function isValid($value)
+    {
+        $sanitize = trim(strip_tags($value));
+        $dt = DateTime::createFromFormat('d/m/Y',$sanitize);
+        
+       
+        // 33/09/75 !== 
+        
+        if($dt && $dt->format('d/m/Y') === $sanitize){
+            return $dt->format('d/m/Y');
+        }else{
+            return false;
+        };
+    }
 
+    public function message()
+    {
+        return 'data non valida';
+    }
 
 }
