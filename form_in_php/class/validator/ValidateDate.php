2e8332890edd1fb208c90d238351f0112621d282
Mona Lisa <sarassoroberto@gmail.com>
2023-03-03 14:26:19 +0100

    refactoring delle classi di validazione

--- form_in_php/class/validator/ValidateDate.php
+++ form_in_php/class/validator/ValidateDate.php
@@ -2,25 +2,48 @@
 
 class ValidateDate implements Validable {
 
+     /** @var string rappresenta il valore immesso nel form ripulito */
+    private $value;
+    private $message;
+
+    /** se il valore è valido e se devo visualizzare il messaggio  */
+    private $valid;
+
+    public function __construct($default_value='',$message='è obbligatorio') {
+        $this->value = $default_value;
+        $this->valid = true;
+        $this->message = $message;
+    }
+
     public function isValid($value)
     {
-        $sanitize = trim(strip_tags($value));
+        $strip_tag = strip_tags($value);
+        $sanitize = trim($strip_tag);
+        
         $dt = DateTime::createFromFormat('d/m/Y',$sanitize);
         if($dt && $dt->format('d/m/Y') === $sanitize) {
 
             return $dt->format('d/m/Y');
         
         }else{
-        
             return false;
-        
         };
     }
     
 
-    public function message()
+    public function getValue()
     {
-        return 'data non valida';
+      return $this->value;
+    }
+   
+    public function getMessage()
+    {
+      return $this->message;
+    }
+   
+    public function getValid()
+    {
+      return $this->valid;
     }
 
 }
