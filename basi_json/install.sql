60ec011315dbcf7f1ea393f8776899f0412bc02c
Mona Lisa <sarassoroberto@gmail.com>
2023-03-07 12:59:54 +0100

    creazione tabella

--- basi_json/install.sql
+++ basi_json/install.sql
@@ -8,14 +8,19 @@ CREATE TABLE regione (
 );
 
 drop table regione;
-
 insert INTO regione (nome) VALUES('Abruzzo');
-
 SELECT * from regione;
-
-
 INSERT INTO regione (nome) VALUES('Valle d\'Aosta/Vallée d\'Aoste');
-
 TRUNCATE TABLE regione;
 
+
+--- Province
+
+CREATE TABLE provincia (
+    provincia_id int NOT NULL AUTO_INCREMENT,
+    nome varchar(255) NOT NULL,
+    sigla CHAR(2) NOT NULL,
+    regione_id  int NULL,
+    PRIMARY KEY (provincia_id)
+);
 
