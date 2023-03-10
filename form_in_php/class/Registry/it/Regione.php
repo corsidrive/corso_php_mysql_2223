10f830ab44ae58fe4da364a41be97e9248a696d9
Mona Lisa <sarassoroberto@gmail.com>
2023-03-10 12:11:20 +0100

    Regione

--- form_in_php/class/Registry/it/Regione.php
+++ form_in_php/class/Registry/it/Regione.php
@@ -9,8 +9,8 @@ class Regione {
             $conn = new PDO(DB_DSN,DB_USER,DB_PASSWORD);
             $stm = $conn->prepare('SELECT * FROM regione;');
             $stm->execute();
-            $result = $stm->fetchAll();
-            print_r($result);
+            $result = $stm->fetchAll(PDO::FETCH_OBJ);
+            return $result;
         } catch (\PDOException $th) {
             throw $th;
         }
--- form_in_php/test/test_regioni.php
+++ form_in_php/test/test_regioni.php
@@ -1,7 +1,10 @@
 <?php
+# php form_in_php/test/test_regioni.php
+require "./config.php";
+require "./form_in_php/class/Registry/it/Regione.php";
 
 // $regioni = new Regioni();
 // $regioni->all(); // Array di (stdClass) regioni
 
 // Metodo Statico può essere utilizzato senza  creare un  istanza 
-Regione::all();
+$regioni = Regione::all();
