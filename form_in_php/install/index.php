3ce4d6960c809a607fd132b3594f820a19ea40a0
Mona Lisa <sarassoroberto@gmail.com>
2023-03-17 14:37:59 +0100

    scrpt installazione

--- form_in_php/install/index.php
+++ form_in_php/install/index.php
@@ -5,6 +5,7 @@ try {
     $conn = new PDO(DB_DSN,DB_USER,DB_PASSWORD);
     $conn->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
     $conn->query($query); 
+    echo "database sincronizzato";
 } catch (\Throwable $th) {
     echo $th->getMessage();
 }
