6d5dbe62924332c35940813a3c6c4de0ba7a0659
Mona Lisa <sarassoroberto@gmail.com>
2023-03-17 14:36:28 +0100

    scrpt installazione

---
+++ form_in_php/install/index.php
@@ -0,0 +1,10 @@
+<?php 
+include "../../config.php";
+$query = file_get_contents("install.sql");
+try {
+    $conn = new PDO(DB_DSN,DB_USER,DB_PASSWORD);
+    $conn->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+    $conn->query($query); 
+} catch (\Throwable $th) {
+    echo $th->getMessage();
+}
