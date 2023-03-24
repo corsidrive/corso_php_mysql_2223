d5ec856bcbd221d15bd50b801fc5b38b7843d918
Mona Lisa <sarassoroberto@gmail.com>
2023-03-24 12:52:50 +0100

    COMMIT

--- form_in_php/index-user.php
+++ form_in_php/index-user.php
@@ -17,12 +17,14 @@ print_r($users);
         <th>Cognome</th> 
         <th>Comune</th>
     </tr>
-    <tr>
-        <td>5</td>
-        <td>Mario</td>
-        <td>Rossi</td>
-        <td>Torino</td>
-    </tr>
+    <?php foreach ($users as $user) { ?>
+        <tr>
+            <td><?= $user->user_id ?></td>
+            <td><?= $user->first_name ?></td>
+            <td><?= $user->last_name ?></td>
+            <td><?= $user->birth_city ?></td>
+        </tr>
+    <?php } ?>
 </table>
 
 <?php require "./class/views/footer-view.php" ?>
