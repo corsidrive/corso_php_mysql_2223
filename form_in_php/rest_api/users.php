b585c9a5c70e48c2b0c192d92a805fa656932289
Mona Lisa <sarassoroberto@gmail.com>
2023-03-29 13:00:09 +0200

    post

--- form_in_php/rest_api/users.php
+++ form_in_php/rest_api/users.php
@@ -1,6 +1,7 @@
 <?php
 
 use crud\UserCRUD;
+use models\User;
 
 include "../../config.php";
 include "../autoload.php";
@@ -25,19 +26,42 @@ switch ($_SERVER['REQUEST_METHOD']) {
 
         $user_id = filter_input(INPUT_GET,'user_id');
         if(!is_null($user_id)){
-            $crud->delete($user_id);
-            $response = [
-                'message' => "user id eliminato con successo",
-                'data' => $user_id
-            ];
+            $rows = $crud->delete($user_id);
+            if($rows == 1){
+                http_response_code(204);
+            }
+
+            if($rows == 0 ){
+            
+                http_response_code(404);
+
+                $response = [
+                    'errors' => [
+                        [
+                            'status' => 404,
+                            'title' => "Utente non trovato",
+                            'details' => $user_id
+                         ]
+                    ]    
+                ];
+            }
+
+           
+            
             echo json_encode($response);
         }
-
-        
-        
         break;
     
-    default:
+    case 'POST' :
+
+        $input = file_get_contents('php://input');
+        $request = json_decode($input,true); // ottengo iun array associativo
+
+        $user = User::arrayToUser($request);
+        $crud->create($user);
+
+        break;
+        default:
         # code...
         break;
 }
