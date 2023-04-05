527645bc4641984de13e2904378373edd36f3078
Mona Lisa <sarassoroberto@gmail.com>
2023-04-05 13:23:00 +0200

    code

--- tests/TaskApiCreateTest.php
+++ tests/TaskApiCreateTest.php
@@ -2,13 +2,12 @@
 use PHPUnit\Framework\TestCase;
 
 require_once "./tests/HTTPClient.php";
-require_once "./config.php";
-class UserApiCreateTest  extends TestCase {
-
-
+class TaskApiCreateTest  extends TestCase {
+    
     public function test_create_user_api()
     {
         (new PDO(DB_DSN,DB_USER,DB_PASSWORD))->query("TRUNCATE TABLE user;"); 
+
         $payload  = [
             "first_name" => "Mirio",
             "last_name" => "DaRoit",
@@ -22,13 +21,12 @@ class UserApiCreateTest  extends TestCase {
         ];
 
       
-       // $response = post("http://localhost/corso_php_mysql_2223/form_in_php/rest_api/users.php",$payload);
+        $response = post("http://localhost/corso_php_mysql_2223/form_in_php/rest_api/users.php",$payload);
         
         //$this->assertNull($response);
-         //$this->assertJson($response);
+         $this->assertJson($response);
  
-        // fwrite(STDERR, print_r($response, TRUE));
+        fwrite(STDERR, print_r($response, TRUE));
     }
 
 }
-
